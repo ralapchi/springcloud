26eecb449051108934edb30fcc3e0c899f87d085
ralapchi <ralapchi@126.com>
2018-07-12 10:46:57 +0800

    配置中心客户端

---
+++ config-client/src/main/java/com/ralapchi/configclient/controller/ConfigController.java
@@ -0,0 +1,18 @@
+package com.ralapchi.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class ConfigController {
+
+    @Value("${foo}")
+    String foo;
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
+}
